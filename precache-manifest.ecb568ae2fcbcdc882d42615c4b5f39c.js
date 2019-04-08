c1388a019537a7e175089fa538c88d1ad5271453
ivan Jankovic <jankoemail@gmail.com>
2019-04-07 20:01:09 -0400

    Updates

---
+++ precache-manifest.ecb568ae2fcbcdc882d42615c4b5f39c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2206172719802cdb41d9",
+    "url": "/static/js/main.2453402b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "2206172719802cdb41d9",
+    "url": "/static/css/main.e1f4678b.chunk.css"
+  },
+  {
+    "revision": "42da3ce7e014a35d72832fa58c25f90f",
+    "url": "/index.html"
+  }
+];
