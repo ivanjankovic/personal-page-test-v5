35f2badc57f98b085667b77deaff24b8daab401e
ivan Jankovic <jankoemail@gmail.com>
2019-04-17 21:55:50 -0400

    navbar mobile done

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 
 import svgLogo from "./svg/navlogo.svg";
+import svgNavIcon from "./svg/navicon.svg";
 
 class NavBar extends React.Component {
   render() {
     return (
       <div>
         <header>
+          <img 
+            src={svgNavIcon} className="source-client-source-2d-Components-NavStyles-NavMenuIcon-1G5H0" alt="navicon" />
           <div className="nav-logowrapper">
             <a href="/">
               <img src={svgLogo} className="navlogo" alt="logo" />
@@ -18,7 +21,7 @@ class NavBar extends React.Component {
             <a href="/made-with-2d" className="navlink">Made with 2D</a>
           </div>
           <div class="account-links">
-            <a href="/signin" class="navlink">Sign In</a>
+            <a href="/signin" class="navlink signinlink">Sign In</a>
           </div>
         </header>
         <div className="dividerLine" />
