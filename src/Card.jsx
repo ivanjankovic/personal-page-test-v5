43e570494e641718f90a896b48b79965070f40fb
ivan Jankovic <jankoemail@gmail.com>
2019-04-19 14:18:48 -0400

    refactored in to dynamic card generation - slopy way

---
+++ src/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+class Card extends React.Component {
+  render() {
+    return (
+      <a href={this.props.href} class="source-client-source-2d-Components-HomeStyles-FeatureBlock3-2eeDh">
+        <div class={`source-client-source-2d-Components-HomeStyles-NewsImage-1xaRn ${this.props.imgClass}`}>
+          {this.props.imgElement}
+        </div>
+        <div class="source-client-source-2d-Components-HomeStyles-NewsContent-3eFrk">
+          <h3>{this.props.h3}</h3>
+          <div class="source-client-source-2d-Components-HomeStyles-NewsDesc-2up89">{this.props.description}</div>
+          <div class="source-client-source-2d-Components-HomeStyles-LearnMore-3RY2c">Learn More</div>
+        </div>
+      </a>
+    )
+  }
+}
+
+export default Card
+
--- src/Main.jsx
+++ src/Main.jsx
@@ -1,14 +1,64 @@
-import React from 'react'
+import React from "react";
 
-import mainPicture from "./main-picture.jpg";
+import Card from "./Card";
+
+const cardsData = {
+  card1: {
+    href: "/made-with-2d",
+    h3: "Made with 2Dimensions",
+    imgElement: <img class="source-client-source-2d-Components-HomeStyles-KovaHero-2iZRR" src="https://cdn.2dimensions.com/features/kova_hero.png" alt=''/>,
+    imgClass: `source-client-source-2d-Components-HomeStyles-MadeWith2dImage-Ctyem`,
+    // imgSource: "https://cdn.2dimensions.com/features/kova_hero.png",
+    // imgAlt: "",
+    description:
+      "Discover real world projects created with our tools. Tag your posts with #MadeWith2D to get featured!"
+  },
+  card2: {
+    href: "https://medium.com/2dimensions/flutter-support-e808cd947e9c",
+    h3: "Flutter Support",
+    imgElement: <div class="source-client-source-2d-Components-HomeStyles-FlutterImage-hG72S"></div>,
+    // imgSource: "https://cdn.2dimensions.com/features/kova_hero.png",
+    // imgAlt: "",
+    description:
+    `Add dynamic real-time animations to Google's mobile app SDK
+    for crafting high-quality native apps.`
+  },
+  card3: {
+    href: "/about-nima",
+    h3: "Nima",
+    imgElement: <img class="source-client-source-2d-Components-HomeStyles-NimaLaptopImg-2ewcg" src="https://www.2dimensions.com/static/2224/images/nima_macbook.png" alt=''/>,
+    // imgSource: "https://cdn.2dimensions.com/features/kova_hero.png",
+    // imgAlt: "",
+    description:
+    `Our original raster animation tool (no vector) for Unity and
+    other game engines.`
+  },
+};
+const Cards = [];
+
+// create cards
+Object.values(cardsData).forEach(card => {
+  Cards.push(
+    <Card
+      href={card.href}
+      h3={card.h3}
+      imgElement={card.imgElement}
+      imgClass={card.imgClass}
+      // imgSource={cardsData.card.imgSource}
+      // imgAlt={cardsData.card.imgAlt}
+      description={card.description}
+    />
+  );
+});
 
 class Main extends React.Component {
   render() {
     return (
       <div>
         <main>
-          {/* <img src={mainPicture} className="main-pic" alt="lions head" /> */}
-          <div class="source-client-source-2d-Components-HomeStyles-FeatureGrid-2-U17"><a href="/made-with-2d" class="source-client-source-2d-Components-HomeStyles-FeatureBlock3-2eeDh"><div class="source-client-source-2d-Components-HomeStyles-NewsImage-1xaRn source-client-source-2d-Components-HomeStyles-MadeWith2dImage-Ctyem"><img class="source-client-source-2d-Components-HomeStyles-KovaHero-2iZRR" src="https://cdn.2dimensions.com/features/kova_hero.png" /></div><div class="source-client-source-2d-Components-HomeStyles-NewsContent-3eFrk"><h3>Made with 2Dimensions</h3><div class="source-client-source-2d-Components-HomeStyles-NewsDesc-2up89">Discover real world projects created with our tools. Tag your posts with #MadeWith2D to get featured!</div><div class="source-client-source-2d-Components-HomeStyles-LearnMore-3RY2c">Learn More</div></div></a><a href="https://medium.com/2dimensions/flutter-support-e808cd947e9c" class="source-client-source-2d-Components-HomeStyles-FeatureBlock3-2eeDh"><div class="source-client-source-2d-Components-HomeStyles-NewsImage-1xaRn"><div class="source-client-source-2d-Components-HomeStyles-FlutterImage-hG72S"></div></div><div class="source-client-source-2d-Components-HomeStyles-NewsContent-3eFrk"><h3>Flutter Support</h3><div class="source-client-source-2d-Components-HomeStyles-NewsDesc-2up89">Add dynamic real-time animations to Google's mobile app SDK for crafting high-quality native apps.</div><div class="source-client-source-2d-Components-HomeStyles-LearnMore-3RY2c">Learn More</div></div></a><a href="/about-nima" class="source-client-source-2d-Components-HomeStyles-FeatureBlock3-2eeDh"><div class="source-client-source-2d-Components-HomeStyles-NewsImage-1xaRn"><img class="source-client-source-2d-Components-HomeStyles-NimaLaptopImg-2ewcg" src="https://www.2dimensions.com/static/2224/images/nima_macbook.png" /></div><div class="source-client-source-2d-Components-HomeStyles-NewsContent-3eFrk"><h3>Nima</h3><div class="source-client-source-2d-Components-HomeStyles-NewsDesc-2up89">Our original raster animation tool (no vector) for Unity and other game engines.</div><div class="source-client-source-2d-Components-HomeStyles-LearnMore-3RY2c">Learn More</div></div></a></div>
+          <div class="source-client-source-2d-Components-HomeStyles-FeatureGrid-2-U17">
+            {Cards}
+          </div>
         </main>
       </div>
     );
