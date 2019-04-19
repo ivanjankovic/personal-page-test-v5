816952f3c209c0b646625ba1681e9324a2ac975f
ivan Jankovic <jankoemail@gmail.com>
2019-04-19 15:15:58 -0400

    card component creaetion and props cleanup

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,8 +4,9 @@ class Card extends React.Component {
   render() {
     return (
       <a href={this.props.href} class="source-client-source-2d-Components-HomeStyles-FeatureBlock3-2eeDh">
-        <div class={`source-client-source-2d-Components-HomeStyles-NewsImage-1xaRn ${this.props.imgClass}`}>
-          {this.props.imgElement}
+        <div class={`source-client-source-2d-Components-HomeStyles-NewsImage-1xaRn ${this.props.imgDivClass}`}>
+          <img class={this.props.imgClass} src={this.props.imgSource} alt={this.props.imgAlt}/>
+          <div class={this.props.divClass}></div>,
         </div>
         <div class="source-client-source-2d-Components-HomeStyles-NewsContent-3eFrk">
           <h3>{this.props.h3}</h3>
--- src/Main.jsx
+++ src/Main.jsx
@@ -6,32 +6,25 @@ const cardsData = {
   card1: {
     href: "/made-with-2d",
     h3: "Made with 2Dimensions",
-    imgElement: <img class="source-client-source-2d-Components-HomeStyles-KovaHero-2iZRR" src="https://cdn.2dimensions.com/features/kova_hero.png" alt=''/>,
-    imgClass: `source-client-source-2d-Components-HomeStyles-MadeWith2dImage-Ctyem`,
-    // imgSource: "https://cdn.2dimensions.com/features/kova_hero.png",
-    // imgAlt: "",
-    description:
-      "Discover real world projects created with our tools. Tag your posts with #MadeWith2D to get featured!"
+    imgDivClass: `source-client-source-2d-Components-HomeStyles-MadeWith2dImage-Ctyem`,
+    imgClass: `source-client-source-2d-Components-HomeStyles-KovaHero-2iZRR`,
+    imgSource: "https://cdn.2dimensions.com/features/kova_hero.png",
+    imgAlt: "",
+    description: "Discover real world projects created with our tools. Tag your posts with #MadeWith2D to get featured!"
   },
   card2: {
     href: "https://medium.com/2dimensions/flutter-support-e808cd947e9c",
     h3: "Flutter Support",
-    imgElement: <div class="source-client-source-2d-Components-HomeStyles-FlutterImage-hG72S"></div>,
-    // imgSource: "https://cdn.2dimensions.com/features/kova_hero.png",
-    // imgAlt: "",
-    description:
-    `Add dynamic real-time animations to Google's mobile app SDK
-    for crafting high-quality native apps.`
+    divClass: `source-client-source-2d-Components-HomeStyles-FlutterImage-hG72S`,
+    description: `Add dynamic real-time animations to Google's mobile app SDK for crafting high-quality native apps.`
   },
   card3: {
     href: "/about-nima",
     h3: "Nima",
-    imgElement: <img class="source-client-source-2d-Components-HomeStyles-NimaLaptopImg-2ewcg" src="https://www.2dimensions.com/static/2224/images/nima_macbook.png" alt=''/>,
-    // imgSource: "https://cdn.2dimensions.com/features/kova_hero.png",
-    // imgAlt: "",
-    description:
-    `Our original raster animation tool (no vector) for Unity and
-    other game engines.`
+    imgClass: `source-client-source-2d-Components-HomeStyles-NimaLaptopImg-2ewcg`,
+    imgSource: "https://www.2dimensions.com/static/2224/images/nima_macbook.png",
+    imgAlt: "",
+    description: `Our original raster animation tool (no vector) for Unity and other game engines.`
   },
 };
 const Cards = [];
@@ -42,10 +35,11 @@ Object.values(cardsData).forEach(card => {
     <Card
       href={card.href}
       h3={card.h3}
-      imgElement={card.imgElement}
+      imgDivClass={card.imgDivClass}
+      divClass={card.divClass}
       imgClass={card.imgClass}
-      // imgSource={cardsData.card.imgSource}
-      // imgAlt={cardsData.card.imgAlt}
+      imgSource={card.imgSource}
+      imgAlt={card.imgAlt}
       description={card.description}
     />
   );
