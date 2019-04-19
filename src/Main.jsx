65e99a0808bcb02caa99663f921128b74367c472
ivan Jankovic <jankoemail@gmail.com>
2019-04-19 17:31:53 -0400

    last card good to go

--- src/Main.jsx
+++ src/Main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 
 import Card from "./Card";
 
+import frogerImg from "./frogger.png"
+
 const cardsData = {
   card1: {
-    href: "/made-with-2d",
+    href: "https://ivanjankovic.github.io/gwg-udacity-fend-p7-neighborhood-map/",
     h3: "Made with 2Dimensions",
     imgDivClass: `source-client-source-2d-Components-HomeStyles-MadeWith2dImage-Ctyem`,
     imgClass: `source-client-source-2d-Components-HomeStyles-KovaHero-2iZRR`,
@@ -13,18 +15,18 @@ const cardsData = {
     description: "Discover real world projects created with our tools. Tag your posts with #MadeWith2D to get featured!"
   },
   card2: {
-    href: "https://medium.com/2dimensions/flutter-support-e808cd947e9c",
+    href: "https://ivanjankovic.github.io/gwg-udacity-fend-p6-my-reads/",
     h3: "Flutter Support",
     divClass: `source-client-source-2d-Components-HomeStyles-FlutterImage-hG72S`,
     description: `Add dynamic real-time animations to Google's mobile app SDK for crafting high-quality native apps.`
   },
-  card3: {
-    href: "/about-nima",
-    h3: "Nima",
+  froggerClone: {
+    href: "https://ivanjankovic.github.io/gwg-udacity-fend-p3-arcade-game/",
+    h3: 'Frogger Clone',
     imgClass: `source-client-source-2d-Components-HomeStyles-NimaLaptopImg-2ewcg`,
-    imgSource: "https://www.2dimensions.com/static/2224/images/nima_macbook.png",
+    imgSource: frogerImg,
     imgAlt: "",
-    description: `Our original raster animation tool (no vector) for Unity and other game engines.`
+    description: `Object-Oriented JavaScript, HTML5 Canvas, Collision Detection and ES6 Modals implementation`
   },
 };
 const Cards = [];
