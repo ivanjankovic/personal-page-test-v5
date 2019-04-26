a88169a4ffc1c11582e20f92abd448d9d91b5456
ivan Jankovic <jankoemail@gmail.com>
2019-04-26 13:45:14 -0400

    footer svg links update

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from "react";
 
-import { ReactComponent as EmailSvg } from "./svg/email.svg";
-import { ReactComponent as RedditSvg } from "./svg/reddit.svg";
-import { ReactComponent as DiscordSvg } from "./svg/discord.svg";
+import { ReactComponent as LinkedInSvg } from "./svg/reddit.svg";
+import { ReactComponent as GitHubSvg } from "./svg/discord.svg";
+import { ReactComponent as MailToSvg } from "./svg/email.svg";
+
+const urlLinkedIn = "https://www.linkedin.com/in/ivanjankovic3";
+const urlGitHub = "https://github.com/ivanjankovic";
+const urlMailTo = "mailto:jankoemail@gmail.com";
 
 class Footer extends Component {
   render() {
@@ -11,9 +15,18 @@ class Footer extends Component {
         <div className="dividerLine" />
         <footer>
           <div>
-            <EmailSvg className="svg-icon" />
-            <RedditSvg className="svg-icon" />
-            <DiscordSvg className="svg-icon" />
+            <LinkedInSvg
+              className="svg-icon"
+              onClick={() => window.open(urlLinkedIn)}
+            />
+            <GitHubSvg
+              className="svg-icon"
+              onClick={() => window.open(urlGitHub)}
+            />
+            <MailToSvg
+              className="svg-icon"
+              onClick={() => window.open(urlMailTo)}
+            />
           </div>
         </footer>
       </div>
